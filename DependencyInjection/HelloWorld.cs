5a9624637be88d994cc082b794812f5371733f0e
Raj <r.kadir@se16.qmul.ac.uk>
2018-10-08 22:43:01 +0100

    added files; HelloWorld.cs and IKernal.cs; implemented basic dependency Injection

---
+++ DependencyInjection/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class HelloWorld
+    {
+        IKernal ikernal = null;
+
+        public HelloWorld(IKernal ikernal)
+        {
+            this.ikernal = ikernal;
+        }
+
+        public void message(string message)
+        {
+            this.ikernal.Hello(message);
+        }
+    }
+}
