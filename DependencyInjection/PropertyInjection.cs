1d92d8ba449dc3bb01184c42a7c1d5235649bcc2
Raj <r.kadir@se16.qmul.ac.uk>
2018-10-08 23:03:22 +0100

    added property injection example

---
+++ DependencyInjection/PropertyInjection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+
+    class PropertyInjection
+    {
+        IKernal kernal = null;
+
+        // property injection
+        public IKernal Kernal
+        {
+            get
+            {
+                return kernal;
+            }
+            set
+            {
+                kernal = value;
+            }
+        }
+
+        public void Hello(string message)
+        {
+            kernal.Hello(message);
+        }
+    }
+}
